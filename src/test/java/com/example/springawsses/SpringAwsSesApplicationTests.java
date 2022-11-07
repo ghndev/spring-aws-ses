d6fba06ae6f86e362050314c46c33d028d093d73
Kwak Hoon <77324409+hooncdn@users.noreply.github.com>
2022-11-07 20:46:43 +0900

    Initial commit

---
+++ src/test/java/com/example/springawsses/SpringAwsSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springawsses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsSesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
